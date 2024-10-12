9042337d29b50ae318b08ac5b3bd8e8f577aaec7
Torsten Wylegala <mail+github@twyleg.de>
2024-10-12 20:02:09 +0000

    Moved cloning and other git actions into this action

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 set -o errexit -o pipefail -o nounset
 
-PACKAGE=$INPUT_PACKAGE
-REPOSITORY=$INPUT_REPOSITORY
+GIT_REPOSITORY=$GIT_REPOSITORY
+GIT_TOKEN=$INPUT_GIT_TOKEN
+PPA_PACKAGE=$INPUT_PPA_PACKAGE
+PPA_REPOSITORY=$INPUT_PPA_REPOSITORY
 UPSTREAM_VERSION=$INPUT_UPSTREAM_VERSION
 GPG_PRIVATE_KEY="$INPUT_GPG_PRIVATE_KEY"
 GPG_PASSPHRASE=$INPUT_GPG_PASSPHRASE
@@ -21,8 +23,8 @@ assert_non_empty() {
     fi
 }
 
-assert_non_empty inputs.package "$PACKAGE"
-assert_non_empty inputs.repository "$REPOSITORY"
+assert_non_empty inputs.ppa_package "$PPA_PACKAGE"
+assert_non_empty inputs.ppa_repository "$PPA_REPOSITORY"
 assert_non_empty inputs.upstream_version "$UPSTREAM_VERSION"
 assert_non_empty inputs.gpg_private_key "$GPG_PRIVATE_KEY"
 assert_non_empty inputs.gpg_passphrase "$GPG_PASSPHRASE"
@@ -31,6 +33,29 @@ assert_non_empty inputs.deb_fullname "$DEB_FULLNAME"
 
 export DEBEMAIL="$DEB_EMAIL"
 export DEBFULLNAME="$DEB_FULLNAME"
+
+export GITMAIL="github-actions[bot]"
+export GITFULLNAME="41898282+github-actions[bot]@users.noreply.github.com"
+export GITURL="https://git:$GIT_TOKEN@github.com/$GIT_REPOSITORY.git"
+
+
+
+echo "::group::Setup git..."
+
+git config --global user.name "$GITFULLNAME"
+git config --global user.email "$GITMAIL"
+git config --global pull.rebase false 
+git config --global --add safe.directory /github/workspace
+
+git clone $GITURL .
+
+git fetch --tags origin debian/latest
+git checkout debian/latest
+git pull --commit --no-edit origin master
+
+echo "::endgroup::"
+
+
 
 echo "::group::Importing GPG private key..."
 echo "Importing GPG private key..."
@@ -47,17 +72,20 @@ fi
 
 echo "::endgroup::"
 
+
+
 echo "::group::Adding PPA..."
-echo "Adding PPA: $REPOSITORY"
-add-apt-repository -y ppa:$REPOSITORY || true
+echo "Adding PPA: $PPA_REPOSITORY"
+add-apt-repository -y ppa:$PPA_REPOSITORY || true
 apt-get update || true
 echo "::endgroup::"
+
+
+
 
 if [[ -z "$SERIES" ]]; then
     SERIES=$(distro-info --supported)
 fi
-
-git config --global --add safe.directory /github/workspace
 
 for s in $SERIES; do
     ubuntu_version=$(distro-info --series $s -r | cut -d' ' -f1)
@@ -67,7 +95,7 @@ for s in $SERIES; do
     changes="New upstream release"
 
     # Update the debian changelog
-    dch --distribution $s --package $PACKAGE --newversion $UPSTREAM_VERSION-$REVISION~ubuntu$ubuntu_version "$changes"
+    dch --distribution $s --package $PPA_PACKAGE --newversion $UPSTREAM_VERSION-$REVISION~ubuntu$ubuntu_version "$changes"
     cat debian/changelog
 
     # Install build dependencies
@@ -81,9 +109,21 @@ for s in $SERIES; do
        -p"gpg --batch --passphrase "$GPG_PASSPHRASE" --pinentry-mode loopback"
 
 
-    dput ppa:$REPOSITORY ../*.changes
+    dput ppa:$PPA_REPOSITORY ../*.changes
 
-    echo "Uploaded $PACKAGE to $REPOSITORY"
+    echo "Uploaded $PPA_PACKAGE to $PPA_REPOSITORY"
 
     echo "::endgroup::"
 done
+
+
+
+echo "::group::Push debian latest branch and version tag"
+
+git add debian/changelog
+git commit -m "New version added"
+git tag debian/$UPSTREAM_VERSION
+git push --tags origin debian/latest
+
+
+echo "::endgroup::"
